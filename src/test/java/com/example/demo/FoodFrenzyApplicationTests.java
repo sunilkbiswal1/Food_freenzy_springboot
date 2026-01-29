64a8270ef6545ea91b01e3fcffb191cf362c0176
sunilkbiswal1 <biswalpranati99@gmail.com>
2026-01-29 15:01:07 +0530

    Add ActiveProfiles annotation to tests
    Added ActiveProfiles annotation for test environment.

--- src/test/java/com/example/demo/FoodFrenzyApplicationTests.java
+++ src/test/java/com/example/demo/FoodFrenzyApplicationTests.java
@@ -2,12 +2,14 @@ package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")  // ← ADD THIS LINE
 class FoodFrenzyApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+        // Test passes if Spring context loads successfully
+    }
 }
